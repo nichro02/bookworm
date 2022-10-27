609c16e508d6b5a3084daae8a3732bd0baa9a570
nichro02 <nichro02@gmail.com>
2022-10-26 19:12:57 -0600

    Linked backend to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,33 @@
 //install dependencies
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const username = process.env.MONGODBUSER;
+const password = process.env.MONGODBPW;
+
+//set up environment
+const MONGODB_URL = `mongodb+srv://${username}:${password}@cluster0.osqwrx5.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+//set up mongoose connection
+
+mongoose
+  .connect(
+    `mongodb+srv://cluster0.osqwrx5.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+      user: process.env.MONGODBUSER,
+      pass: process.env.MONGODBPW,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
+  .catch((error) => console.log(error.message))
